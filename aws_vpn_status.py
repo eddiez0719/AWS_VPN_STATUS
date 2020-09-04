d79162a8ac33a2ccca3902b3796ab5f9d82f4e9c
EddieZ <67562825+eddiez0719@users.noreply.github.com>
2020-09-04 17:12:24 +1000

    Update aws_vpn_status.py

--- aws_vpn_status.py
+++ aws_vpn_status.py
@@ -14,36 +14,33 @@ def a_function():
             }
         ]
     )
-    # print(res)
-    i = res["VpnConnections"]
-    for l in i:
-        # print(l)
-        print('-------------------------')
-        print('\nCGWId: {}, Clinic: {}, State: {}, Type: {}, VPNConnectionID: {}, AWS Tunnel1 IP: {}, Tunnel_1 Status: {}, AWS Tunnel2 IP: {}, Tunnel_2 Status: {}'.format(l['CustomerGatewayId'], l['Tags'][0]['Value'], l['State'], l['Type'], l['VpnConnectionId'],
-                '\n' + l['VgwTelemetry'][0]['OutsideIpAddress'], l['VgwTelemetry'][0]['Status'],
-                l['VgwTelemetry'][1]['OutsideIpAddress'], l['VgwTelemetry'][1]['Status']))
+    i = res
+    for l in i["VpnConnections"]:
+        #print(l['Tags'])
+        for key in l['Tags']:
+            kl = (key['Value']).lower()
+            #print(kl)
+            print('\nCGWId: {}, Clinic: {}, State: {}, Type: {}, VPNConnectionID: {}, AWS Tunnel1 IP: {}, Tunnel_1 Status: {}, AWS Tunnel2 IP: {}, Tunnel_2 Status: {}'.format(l['CustomerGatewayId'], kl, l['State'], l['Type'], l['VpnConnectionId'], '\n' + l['VgwTelemetry'][0]['OutsideIpAddress'], l['VgwTelemetry'][0]['Status'], l['VgwTelemetry'][1]['OutsideIpAddress'], l['VgwTelemetry'][1]['Status']))
+            if kl not in ign:
+                if l['VgwTelemetry'][0]['Status'] == 'DOWN' and l['VgwTelemetry'][1]['Status'] == 'DOWN':
+                    print('{} VPN is Down'.format(kl))
+                    
+                    smtpsvr = 'smtp.office365.com'
+                    smtpserver = smtplib.SMTP(smtpsvr, 587)
 
-        if l['Tags'][0]['Value'] not in ign:
+                    msg = EmailMessage()
+                    msg['Subject'] = '{} VPN is Down'.format(kl)
+                    msg['From'] = 'c@xxx.com.au'
+                    msg['To'] = 'a@xxx.com.au'
 
-            if l['VgwTelemetry'][0]['Status'] == 'DOWN' and l['VgwTelemetry'][1]['Status'] == 'DOWN':
-                print('{} VPN is Down'.format(l['Tags'][0]['Value']))
+                    smtpserver.ehlo()
+                    smtpserver.starttls()
+                    smtpserver.login('c@xxx.com.au', 'password')
+                    smtpserver.send_message(msg)
+                    smtpserver.close()
 
-                smtpsvr = 'smtp.office365.com'
-                smtpserver = smtplib.SMTP(smtpsvr, 587)
-
-                msg = EmailMessage()
-                msg['Subject'] = '{} VPN is Down'.format(l['Tags'][0]['Value'])
-                msg['From'] = 'c@xxx.com.au'
-                msg['To'] = 'a@xxx.com.au'
-
-                smtpserver.ehlo()
-                smtpserver.starttls()
-                smtpserver.login('c@xxx.com.au', 'password')
-                smtpserver.send_message(msg)
-                smtpserver.close()
-
-            else:
-                pass
+                else:
+                    pass
 
 
 a_function()
